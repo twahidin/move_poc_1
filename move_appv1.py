4a8ea447677dc5fdcf8a45923e1a24ab00ccccc3
Joe Tay <joetay@joes-mbp.lan>
2021-09-15 19:22:34 +0800

    try basic

--- move_appv1.py
+++ move_appv1.py
@@ -1,54 +1,17 @@
-import threading
-from typing import Union
-
+import cv2
 import av
-import numpy as np
-import streamlit as st
-
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import VideoProcessorBase, webrtc_streamer
 
 
-def main():
-    class VideoTransformer(VideoTransformerBase):
-        frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
-        in_image: Union[np.ndarray, None]
-        out_image: Union[np.ndarray, None]
+class VideoTransformer(VideoProcessorBase):
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        
+        in_image = frame.to_ndarray(format="bgr24")
+        
+        return av.VideoFrame.from_ndarray(in_image, format="bgr24")
 
-        def __init__(self) -> None:
-            self.frame_lock = threading.Lock()
-            self.in_image = None
-            self.out_image = None
+webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
 
-        def transform(self, frame: av.VideoFrame) -> np.ndarray:
-            in_image = frame.to_ndarray(format="bgr24")
-
-            out_image = in_image[:, ::-1, :]  # Simple flipping for example.
-
-            with self.frame_lock:
-                self.in_image = in_image
-                self.out_image = out_image
-
-            return out_image
-
-    ctx = webrtc_streamer(key="snapshot", video_transformer_factory=VideoTransformer)
-
-    if ctx.video_transformer:
-        if st.button("Snapshot"):
-            with ctx.video_transformer.frame_lock:
-                in_image = ctx.video_transformer.in_image
-                out_image = ctx.video_transformer.out_image
-
-            if in_image is not None and out_image is not None:
-                st.write("Input image:")
-                st.image(in_image, channels="BGR")
-                st.write("Output image:")
-                st.image(out_image, channels="BGR")
-            else:
-                st.warning("No frames available yet.")
-
-
-if __name__ == "__main__":
-    main()
 
 
 
